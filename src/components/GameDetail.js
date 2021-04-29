5a2cafb9f2c84743336725ba3864951a1c2fc37d
Tolga Kara <tolgakara2.0@gmail.com>
2021-04-29 14:01:43 +0200

    Added Platform Icon and Star Rating

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -6,6 +6,13 @@ import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 
+// Platforms Icons
+import { FaPlaystation, FaSteam, FaXbox, FaGamepad } from "react-icons/fa";
+import { SiNintendoswitch, SiApple, SiPlaystation3, SiPlaystation4 } from "react-icons/si";
+
+// Star Icons
+import { TiStarFullOutline, TiStarHalfOutline, TiStarOutline } from "react-icons/ti";
+
 const GameDetail = ({ pathId }) => {
 	const history = useHistory();
 	// Exit Detail
@@ -14,6 +21,43 @@ const GameDetail = ({ pathId }) => {
 		if (element.classList.contains("shadow")) {
 			document.body.style.overflow = "auto";
 			history.push("/");
+		}
+	};
+
+	// Get Rating in Star Icons
+	const getStarIcon = () => {
+		const stars = [];
+		const rating = Math.floor(game.rating);
+		for (let index = 1; index <= 5; index++) {
+			if (index <= rating) {
+				stars.push(<TiStarFullOutline />);
+			} else {
+				stars.push(<TiStarOutline />);
+			}
+		}
+		return stars;
+	};
+
+	// Getting the right Icon for the right Platform
+	const getPlatformIcon = (platform) => {
+		switch (platform) {
+			case "PlayStation 5":
+				return <FaPlaystation />;
+			case "PlayStation 4":
+				return <SiPlaystation4 />;
+			case "PlayStation 3":
+				return <SiPlaystation3 />;
+			case "Xbox One" || "Xbox 360" || "Xbox One" || "Xbox Series S/X":
+				return <FaXbox />;
+			case "PC":
+				return <FaSteam />;
+			case "Nintendo Switch":
+				return <SiNintendoswitch />;
+			case "iOS":
+				return <SiApple />;
+
+			default:
+				return <FaGamepad />;
 		}
 	};
 
@@ -27,13 +71,12 @@ const GameDetail = ({ pathId }) => {
 							<div className='rating'>
 								<motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
 								<p>Rating: {game.rating}</p>
+								{getStarIcon()}
 							</div>
 							<Info>
 								<h3>Platforms</h3>
 								<Platforms>
-									{game.platforms.map((data) => (
-										<h3 key={data.platform.id}>{data.platform.name}</h3>
-									))}
+									{game.platforms.map((data) => getPlatformIcon(data.platform.name))}
 								</Platforms>
 							</Info>
 						</Stats>
@@ -63,6 +106,7 @@ const CardShadow = styled(motion.div)`
 	position: fixed;
 	top: 0;
 	left: 0;
+	z-index: 5;
 	&::-webkit-scrollbar {
 		width: 0.5rem;
 	}
@@ -81,6 +125,7 @@ const Detail = styled(motion.div)`
 	padding: 2rem 5rem;
 	background: #fff;
 	position: absolute;
+	z-index: 10;
 	left: 10%;
 	color: #000;
 	img {
@@ -92,6 +137,11 @@ const Stats = styled(motion.div)`
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
+	svg {
+		width: 2rem;
+		height: 2rem;
+		display: inline;
+	}
 `;
 
 const Info = styled(motion.div)`
